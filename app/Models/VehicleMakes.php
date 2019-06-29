77085258a7d4e33b7c6d250ed5ea23939f1ab5b1
Hung Truong <hungtruong@Hungs-MacBook-Pro.local>
2019-06-28 21:53:10 -0500

    2019-06-28: Set up relationship between vehicle_make and vehicle_model

--- app/Models/VehicleMakes.php
+++ app/Models/VehicleMakes.php
@@ -6,4 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class VehicleMakes extends Model {
   protected $guarded = ['id'];
+
+  public function models() {
+    return $this->hasMany(VehicleModels::class);
+  }
 }
