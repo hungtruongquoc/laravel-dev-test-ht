c92fc4ccf6295f59e2fff723b7ef5df74b88a721
Hung Truong <hungtruong@Hungs-MacBook-Pro.local>
2019-06-29 00:15:22 -0500

    2019-06-29: Added new styling for create form

---
+++ resources/views/select.blade.php
@@ -0,0 +1,8 @@
+<label for="{{$id}}">{{$title}}</label>
+<select class="form-control" id="{{$id}}" name="{{$name}}" @if(isset($disabled) && $disabled) disabled @endif
+  @if(isset($autofocus) && $autofocus) autofocus @endif
+>
+ @foreach($items as $item)
+   <option value="{{$item->id}}">{{$item->title}}</option>
+  @endforeach
+</select>
